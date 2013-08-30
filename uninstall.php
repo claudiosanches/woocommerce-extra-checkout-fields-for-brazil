93e37254ef96ac48b5ced9e6f9c5a766e8d3d854
claudiosmweb <contato@claudiosmweb.com>
2013-08-30 18:41:42 -0300

    fixed standards

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,7 @@
 <?php
 
 // If uninstall not called from WordPress exit.
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) || !WP_UNINSTALL_PLUGIN || dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) ) ) {
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) || ! WP_UNINSTALL_PLUGIN || dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) ) ) {
 
     status_header( 404 );
     exit;
