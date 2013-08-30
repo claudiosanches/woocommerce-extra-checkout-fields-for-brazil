31c7931227fc835968f41689f90f8165407745d0
claudiosmweb <contato@claudiosmweb.com>
2013-08-30 18:41:13 -0300

    added autocomplete onload and support to Chosen

--- js/jquery.address.autocomplete.js
+++ js/jquery.address.autocomplete.js
@@ -4,36 +4,60 @@ jQuery(document).ready(function($) {
      * Address Autocomplete.
      *
      * @param  {string} field Field name.
-     * @return Autocomplete fields.
+     *
+     * @return {void}         Autocomplete fields.
      */
     function addressAutoComplete(field) {
+        // Valid CEP.
+        var cep = $('#' + field + '_postcode').val().replace('.','').replace('-', '');
+        var country = $('#' + field + '_country').val();
+
+        // Check country is BR.
+        if (cep !== '' && 8 === cep.length && 'BR' === country) {
+            // Gets the address.
+            $.getScript('http://www.toolsweb.com.br/webservice/clienteWebService.php?cep=' + cep + '&formato=javascript', function() {
+
+                // Address.
+                if ('' !== unescape(resultadoCEP['tipoLogradouro'])) {
+                    $('#' + field + '_address_1').val(unescape(resultadoCEP['tipoLogradouro']) + ' ' + unescape(resultadoCEP['logradouro']));
+                } else {
+                    $('#' + field + '_address_1').val(unescape(resultadoCEP['logradouro']));
+                }
+
+                // Neighborhood.
+                $('#' + field + '_neighborhood').val(unescape(resultadoCEP['bairro']));
+
+                // City.
+                $('#' + field + '_city').val(unescape(resultadoCEP['cidade']));
+
+                // State.
+                $('#' + field + '_state option:selected').attr('selected', false);
+                $('#' + field + '_state option[value="' + unescape(resultadoCEP['estado']) + '"]').attr('selected', true);
+
+                // Chosen support.
+                $('#' + field + '_state').trigger("liszt:updated");
+            });
+        }
+    }
+
+    /**
+     * Autocomplete the Address on change the *_postcode input.
+     *
+     * @param  {string} field Field name.
+     *
+     * @return {void}         Autocomplete fields.
+     */
+    function addressAutoCompleteOnChange(field) {
         $('#' + field + '_postcode').on('blur', function() {
-            // Valid CEP.
-            var cep = $(this).val().replace('.','').replace('-', '');
-            var country = $('#' + field + '_country').val();
-
-            // Check country is BR.
-            if (cep !== '' && cep.length === 8 && country === 'BR') {
-                $.getScript('http://www.toolsweb.com.br/webservice/clienteWebService.php?cep=' + cep + '&formato=javascript', function() {
-
-                    if (unescape(resultadoCEP['tipoLogradouro']) !== '') {
-                        $('#' + field + '_address_1').val(unescape(resultadoCEP['tipoLogradouro']) + ' ' + unescape(resultadoCEP['logradouro']));
-                    }
-                    else {
-                        $('#' + field + '_address_1').val(unescape(resultadoCEP['logradouro']));
-                    }
-                    $('#' + field + '_neighborhood').val(unescape(resultadoCEP['bairro']));
-                    $('#' + field + '_city').val(unescape(resultadoCEP['cidade']));
-                    $('#' + field + '_state').val(unescape(resultadoCEP['estado']));
-
-                });
-            }
+            addressAutoComplete(field);
         });
     }
 
     // Auto complete billing address.
     addressAutoComplete('billing');
+    addressAutoCompleteOnChange('billing');
 
     // Auto complete shipping address.
     addressAutoComplete('shipping');
+    addressAutoCompleteOnChange('shipping');
 });
