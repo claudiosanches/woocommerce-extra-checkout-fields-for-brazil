bfba1769e493d84458cf1f8291807446eb894bcc
claudiosmweb <contato@claudiosmweb.com>
2014-02-23 18:54:04 -0300

    improve the checkout masks

--- public/assets/js/checkout.masks.js
+++ public/assets/js/checkout.masks.js
@@ -12,16 +12,20 @@
 			$( '#billing_cpf, #credit-card-cpf' ).mask( '999.999.999-99' );
 
 			// RG.
-			$( '#billing_rg' ).mask( '99.999.999-9' );
+			$( '#billing_rg' ).keydown(function (e) {
+				if ( $.inArray( e.keyCode, [46, 8, 13] ) !== -1 || ( e.keyCode === 65 && e.ctrlKey === true ) || ( e.keyCode >= 35 && e.keyCode <= 39 ) ) {
+					return;
+				}
+				if ( ( e.shiftKey || ( e.keyCode < 48 || e.keyCode > 57 ) ) && ( e.keyCode < 96 || e.keyCode > 105 ) ) {
+					e.preventDefault();
+				}
+			});
 
 			// CPNJ.
 			$( '#billing_cnpj' ).mask( '99.999.999/9999-99' );
 
-			// Phone.
-			$( '#billing_phone' ).mask( '(99) 9999-9999' );
-
 			// Cell phone.
-			$( '#billing_cellphone, #credit-card-phone' ).focusout( function () {
+			$( '#billing_phone, #billing_cellphone, #credit-card-phone' ).focusout( function () {
 				var phone, element;
 				element = $( this );
 				element.unmask();
