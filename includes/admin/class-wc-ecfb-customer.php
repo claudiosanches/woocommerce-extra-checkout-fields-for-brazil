646c59ef06a719ed32dd9bd519e985be3881ab3d
Claudio Sanches <contato@claudiosmweb.com>
2015-09-07 23:36:44 -0300

    Fixed user profile select2 fields, closes #9

--- includes/admin/class-wc-ecfb-customer.php
+++ includes/admin/class-wc-ecfb-customer.php
@@ -25,32 +25,24 @@ class Extra_Checkout_Fields_For_Brazil_Customer {
 	 * @return array         Custom fields.
 	 */
 	public function customer_meta_fields( $fields ) {
-		unset( $fields );
-
 		// Get plugin settings.
 		$settings = get_option( 'wcbcf_settings' );
 
 		// Billing fields.
-		$fields['billing']['title'] = __( 'Customer Billing Address', 'woocommerce-extra-checkout-fields-for-brazil' );
-		$fields['billing']['fields']['billing_first_name'] = array(
-			'label' => __( 'First name', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['billing']['fields']['billing_last_name'] = array(
-			'label' => __( 'Last name', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
+		$new_fields['billing']['title'] = __( 'Customer Billing Address', 'woocommerce-extra-checkout-fields-for-brazil' );
+		$new_fields['billing']['fields']['billing_first_name'] = $fields['billing']['fields']['billing_first_name'];
+		$new_fields['billing']['fields']['billing_last_name']  = $fields['billing']['fields']['billing_last_name'];
 
 		if ( 0 != $settings['person_type'] ) {
 
 			if ( 1 == $settings['person_type'] || 2 == $settings['person_type'] ) {
-				$fields['billing']['fields']['billing_cpf'] = array(
+				$new_fields['billing']['fields']['billing_cpf'] = array(
 					'label' => __( 'CPF', 'woocommerce-extra-checkout-fields-for-brazil' ),
 					'description' => ''
 				);
 
 				if ( isset( $settings['rg'] ) ) {
-					$fields['billing']['fields']['billing_rg'] = array(
+					$new_fields['billing']['fields']['billing_rg'] = array(
 						'label' => __( 'RG', 'woocommerce-extra-checkout-fields-for-brazil' ),
 						'description' => ''
 					);
@@ -58,137 +50,80 @@ class Extra_Checkout_Fields_For_Brazil_Customer {
 			}
 
 			if ( 1 == $settings['person_type'] || 3 == $settings['person_type'] ) {
-				$fields['billing']['fields']['billing_company'] = array(
-					'label' => __( 'Company Name', 'woocommerce-extra-checkout-fields-for-brazil' ),
-					'description' => ''
-				);
-				$fields['billing']['fields']['billing_cnpj'] = array(
+				$new_fields['billing']['fields']['billing_company'] = $fields['billing']['fields']['billing_company'];
+				$new_fields['billing']['fields']['billing_cnpj'] = array(
 					'label' => __( 'CNPJ', 'woocommerce-extra-checkout-fields-for-brazil' ),
 					'description' => ''
 				);
 
 				if ( isset( $settings['ie'] ) ) {
-					$fields['billing']['fields']['billing_ie'] = array(
+					$new_fields['billing']['fields']['billing_ie'] = array(
 						'label' => __( 'State Registration', 'woocommerce-extra-checkout-fields-for-brazil' ),
 						'description' => ''
 					);
 				}
 			}
 		} else {
-			$fields['billing']['fields']['billing_company'] = array(
-				'label' => __( 'Company', 'woocommerce-extra-checkout-fields-for-brazil' ),
-				'description' => ''
-			);
+			$new_fields['billing']['fields']['billing_company'] = $fields['billing']['fields']['billing_company'];
 		}
 
 		if ( isset( $settings['birthdate_sex'] ) ) {
-			$fields['billing']['fields']['billing_birthdate'] = array(
+			$new_fields['billing']['fields']['billing_birthdate'] = array(
 				'label' => __( 'Birthdate', 'woocommerce-extra-checkout-fields-for-brazil' ),
 				'description' => ''
 			);
-			$fields['billing']['fields']['billing_sex'] = array(
+			$new_fields['billing']['fields']['billing_sex'] = array(
 				'label' => __( 'Sex', 'woocommerce-extra-checkout-fields-for-brazil' ),
 				'description' => ''
 			);
 		}
 
-		$fields['billing']['fields']['billing_country'] = array(
-			'label' => __( 'Country', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => __( '2 letter Country code', 'woocommerce-extra-checkout-fields-for-brazil' )
-		);
-		$fields['billing']['fields']['billing_postcode'] = array(
-			'label' => __( 'Postcode', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['billing']['fields']['billing_address_1'] = array(
-			'label' => __( 'Address 1', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['billing']['fields']['billing_number'] = array(
+		$new_fields['billing']['fields']['billing_address_1'] = $fields['billing']['fields']['billing_address_1'];
+		$new_fields['billing']['fields']['billing_number'] = array(
 			'label' => __( 'Number', 'woocommerce-extra-checkout-fields-for-brazil' ),
 			'description' => ''
 		);
-		$fields['billing']['fields']['billing_address_2'] = array(
-			'label' => __( 'Address 2', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['billing']['fields']['billing_neighborhood'] = array(
+		$new_fields['billing']['fields']['billing_address_2'] = $fields['billing']['fields']['billing_address_2'];
+		$new_fields['billing']['fields']['billing_neighborhood'] = array(
 			'label' => __( 'Neighborhood', 'woocommerce-extra-checkout-fields-for-brazil' ),
 			'description' => ''
 		);
-		$fields['billing']['fields']['billing_city'] = array(
-			'label' => __( 'City', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['billing']['fields']['billing_state'] = array(
-			'label' => __( 'State', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => __( 'State code', 'woocommerce-extra-checkout-fields-for-brazil' )
-		);
-		$fields['billing']['fields']['billing_phone'] = array(
-			'label' => __( 'Telephone', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
+		$new_fields['billing']['fields']['billing_city']     = $fields['billing']['fields']['billing_city'];
+		$new_fields['billing']['fields']['billing_postcode'] = $fields['billing']['fields']['billing_postcode'];
+		$new_fields['billing']['fields']['billing_country']  = $fields['billing']['fields']['billing_country'];
+		$new_fields['billing']['fields']['billing_state']    = $fields['billing']['fields']['billing_state'];
+		$new_fields['billing']['fields']['billing_phone']    = $fields['billing']['fields']['billing_phone'];
 
 		if ( isset( $settings['cell_phone'] ) ) {
-			$fields['billing']['fields']['billing_cellphone'] = array(
+			$new_fields['billing']['fields']['billing_cellphone'] = array(
 				'label' => __( 'Cell Phone', 'woocommerce-extra-checkout-fields-for-brazil' ),
 				'description' => ''
 			);
 		}
 
-		$fields['billing']['fields']['billing_email'] = array(
-			'label' => __( 'Email', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
+		$new_fields['billing']['fields']['billing_email'] = $fields['billing']['fields']['billing_email'];
 
 		// Shipping fields.
-		$fields['shipping']['title'] = __( 'Customer Shipping Address', 'woocommerce-extra-checkout-fields-for-brazil' );
-		$fields['shipping']['fields']['shipping_first_name'] = array(
-			'label' => __( 'First name', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['shipping']['fields']['shipping_last_name'] = array(
-			'label' => __( 'Last name', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['shipping']['fields']['shipping_company'] = array(
-			'label' => __( 'Company', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['shipping']['fields']['shipping_country'] = array(
-			'label' => __( 'Country', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => __( '2 letter Country code', 'woocommerce-extra-checkout-fields-for-brazil' )
-		);
-		$fields['shipping']['fields']['shipping_postcode'] = array(
-			'label' => __( 'Postcode', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['shipping']['fields']['shipping_address_1'] = array(
-			'label' => __( 'Address 1', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['shipping']['fields']['shipping_number'] = array(
+		$new_fields['shipping']['title'] = __( 'Customer Shipping Address', 'woocommerce-extra-checkout-fields-for-brazil' );
+		$new_fields['shipping']['fields']['shipping_first_name'] = $fields['shipping']['fields']['shipping_first_name'];
+		$new_fields['shipping']['fields']['shipping_last_name']  = $fields['shipping']['fields']['shipping_last_name'];
+		$new_fields['shipping']['fields']['shipping_company']    = $fields['shipping']['fields']['shipping_company'];
+		$new_fields['shipping']['fields']['shipping_address_1']  = $fields['shipping']['fields']['shipping_address_1'];
+		$new_fields['shipping']['fields']['shipping_number'] = array(
 			'label' => __( 'Number', 'woocommerce-extra-checkout-fields-for-brazil' ),
 			'description' => ''
 		);
-		$fields['shipping']['fields']['shipping_address_2'] = array(
-			'label' => __( 'Address 2', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['shipping']['fields']['shipping_neighborhood'] = array(
+		$new_fields['shipping']['fields']['shipping_address_2']  = $fields['shipping']['fields']['shipping_address_2'];
+		$new_fields['shipping']['fields']['shipping_neighborhood'] = array(
 			'label' => __( 'Neighborhood', 'woocommerce-extra-checkout-fields-for-brazil' ),
 			'description' => ''
 		);
-		$fields['shipping']['fields']['shipping_city'] = array(
-			'label' => __( 'City', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => ''
-		);
-		$fields['shipping']['fields']['shipping_state'] = array(
-			'label' => __( 'State', 'woocommerce-extra-checkout-fields-for-brazil' ),
-			'description' => __( 'State code', 'woocommerce-extra-checkout-fields-for-brazil' )
-		);
+		$new_fields['shipping']['fields']['shipping_city']     = $fields['shipping']['fields']['shipping_city'];
+		$new_fields['shipping']['fields']['shipping_postcode'] = $fields['shipping']['fields']['shipping_postcode'];
+		$new_fields['shipping']['fields']['shipping_country']  = $fields['shipping']['fields']['shipping_country'];
+		$new_fields['shipping']['fields']['shipping_state']    = $fields['shipping']['fields']['shipping_state'];
 
-		$new_fields = apply_filters( 'wcbcf_customer_meta_fields', $fields );
+		$new_fields = apply_filters( 'wcbcf_customer_meta_fields', $new_fields );
 
 		return $new_fields;
 	}
