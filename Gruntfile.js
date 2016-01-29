01686934af87387685a51cccd98adb92b77aa1dc
Claudio Sanches <contato@claudiosmweb.com>
2016-01-29 16:05:06 -0200

    Updated grunt dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
 /*jshint node:true */
+var expandHomeDir = require( 'expand-home-dir' );
+
 module.exports = function( grunt ) {
 'use strict';
 
@@ -25,7 +27,7 @@ module.exports = function( grunt ) {
 
 		// svn settings
 		svn_settings: {
-			path: '../../../../wp_plugins/<%= pkg.name %>',
+			path: expandHomeDir( '~/Projects/wordpress-plugins-svn/' ) + '<%= pkg.name %>',
 			tag: '<%= svn_settings.path %>/tags/<%= pkg.version %>',
 			trunk: '<%= svn_settings.path %>/trunk',
 			exclude: [
