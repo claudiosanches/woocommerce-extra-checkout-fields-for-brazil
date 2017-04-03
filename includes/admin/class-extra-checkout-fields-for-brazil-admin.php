9f577454e62743e41d481df4456d4de64658f5a4
Claudio Sanches <contato@claudiosanches.com>
2017-04-03 01:49:08 -0300

    Fixed coding standards in admin class

--- includes/admin/class-extra-checkout-fields-for-brazil-admin.php
+++ includes/admin/class-extra-checkout-fields-for-brazil-admin.php
@@ -1,10 +1,16 @@
 <?php
+/**
+ * Extra checkout fields admin.
+ *
+ * @package Extra_Checkout_Fields_For_Brazil/Admin
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
 /**
- * Plugin admin class.
+ * Extra_Checkout_Fields_For_Brazil_Admin class.
  */
 class Extra_Checkout_Fields_For_Brazil_Admin {
 
@@ -25,7 +31,7 @@ class Extra_Checkout_Fields_For_Brazil_Admin {
 		$screen = get_current_screen();
 		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
-		if ( 'shop_order' == $screen->id ) {
+		if ( 'shop_order' === $screen->id ) {
 			// Get plugin settings.
 			$settings = get_option( 'wcbcf_settings' );
 
@@ -33,7 +39,7 @@ class Extra_Checkout_Fields_For_Brazil_Admin {
 			wp_enqueue_style( 'woocommerce-extra-checkout-fields-for-brazil-admin', Extra_Checkout_Fields_For_Brazil::get_assets_url() . 'css/admin/admin.css', array(), Extra_Checkout_Fields_For_Brazil::VERSION );
 
 			// Shop order.
-			wp_enqueue_script( 'woocommerce-extra-checkout-fields-for-brazil-shop-order', Extra_Checkout_Fields_For_Brazil::get_assets_url() . 'js/admin/shop-order' . $suffix .'.js', array( 'jquery' ), Extra_Checkout_Fields_For_Brazil::VERSION, true );
+			wp_enqueue_script( 'woocommerce-extra-checkout-fields-for-brazil-shop-order', Extra_Checkout_Fields_For_Brazil::get_assets_url() . 'js/admin/shop-order' . $suffix . '.js', array( 'jquery' ), Extra_Checkout_Fields_For_Brazil::VERSION, true );
 
 			// Localize strings.
 			wp_localize_script(
@@ -42,13 +48,13 @@ class Extra_Checkout_Fields_For_Brazil_Admin {
 				array(
 					'load_message' => esc_js( __( 'Load the customer extras data?', 'woocommerce-extra-checkout-fields-for-brazil' ) ),
 					'copy_message' => esc_js( __( 'Also copy the data of number and neighborhood?', 'woocommerce-extra-checkout-fields-for-brazil' ) ),
-					'person_type'  => absint( $settings['person_type'] )
+					'person_type'  => absint( $settings['person_type'] ),
 				)
 			);
 		}
 
-		if ( 'woocommerce_page_woocommerce-extra-checkout-fields-for-brazil' == $screen->id ) {
-			wp_enqueue_script( 'woocommerce-extra-checkout-fields-for-brazil-admin', Extra_Checkout_Fields_For_Brazil::get_assets_url() . 'js/admin/admin' . $suffix .'.js', array( 'jquery' ), Extra_Checkout_Fields_For_Brazil::VERSION );
+		if ( 'woocommerce_page_woocommerce-extra-checkout-fields-for-brazil' === $screen->id ) {
+			wp_enqueue_script( 'woocommerce-extra-checkout-fields-for-brazil-admin', Extra_Checkout_Fields_For_Brazil::get_assets_url() . 'js/admin/admin' . $suffix . '.js', array( 'jquery' ), Extra_Checkout_Fields_For_Brazil::VERSION );
 		}
 	}
 
@@ -56,7 +62,9 @@ class Extra_Checkout_Fields_For_Brazil_Admin {
 	 * Maybe install.
 	 */
 	public function maybe_install() {
-		if ( $version = get_option( 'wcbcf_version' ) ) {
+		$version = get_option( 'wcbcf_version' );
+
+		if ( $version ) {
 			if ( version_compare( $version, Extra_Checkout_Fields_For_Brazil::VERSION, '<' ) ) {
 				$options = get_option( 'wcbcf_settings' );
 
@@ -75,16 +83,12 @@ class Extra_Checkout_Fields_For_Brazil_Admin {
 		} else {
 			$default = array(
 				'person_type'     => 1,
-				// 'only_brazil'     => 0,
-				// 'ie'              => 0,
-				// 'rg'              => 0,
-				// 'birthdate_sex'   => 0,
 				'cell_phone'      => 1,
 				'mailcheck'       => 1,
 				'maskedinput'     => 1,
 				'addresscomplete' => 1,
 				'validate_cpf'    => 1,
-				'validate_cnpj'   => 1
+				'validate_cnpj'   => 1,
 			);
 
 			add_option( 'wcbcf_settings', $default );
