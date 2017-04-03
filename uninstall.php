6631771b9fd25db7dfb4557a2ab9fe66d2ed33a9
Claudio Sanches <contato@claudiosanches.com>
2017-04-03 03:13:33 -0300

    Fixed coding standards in uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Uninstall file.
+ *
+ * @package Extra_Checkout_Fields_For_Brazil/Uninstall
+ */
+
 // If uninstall not called from WordPress exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
