a3de1e849af33effde7d981cd1618ad70189cdc7
Claudio Sanches <contato@claudiosanches.com>
2019-09-22 21:24:56 -0300

    Initial implementation to work with PHP 5.6

--- woocommerce-extra-checkout-fields-for-brazil.php
+++ woocommerce-extra-checkout-fields-for-brazil.php
@@ -5,15 +5,15 @@
  * Description: Adds new checkout fields, field masks and other things necessary to properly work with WooCommerce on Brazil.
  * Author:      Claudio Sanches
  * Author URI:  https://claudiosanches.com
- * Version:     3.7.0
- * License:     GPLv2 or later
+ * Version:     4.0.0-dev
+ * License:     GPL-3.0+
  * Text Domain: woocommerce-extra-checkout-fields-for-brazil
  * Domain Path: /languages
  *
  * Brazilian Market on WooCommerce is free software: you can
  * redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation,
- * either version 2 of the License, or any later version.
+ * either version 3 of the License, or any later version.
  *
  * Brazilian Market on WooCommerce is distributed in the hope
  * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
@@ -22,136 +22,45 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Brazilian Market on WooCommerce. If not, see
- * <https://www.gnu.org/licenses/gpl-2.0.txt>.
+ * <https://www.gnu.org/licenses/gpl-3.0.txt>.
  *
- * @package Extra_Checkout_Fields_For_Brazil
+ * @package ClaudioSanches\BrazilianMarket
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+if ( version_compare( PHP_VERSION, '5.6.0', '<' ) ) {
+	/**
+	 * Function compatible with PHP 5.2 to display a notice about unsupported
+	 * php version.
+	 *
+	 * @return void
+	 */
+	function cs_brazilian_market_unsupported_php_version_notice() {
+		$plugin_name = plugin_basename( __FILE__ );
+		$disable_url = wp_nonce_url(
+			add_query_arg(
+				array(
+					'action'        => 'deactivate',
+					'plugin'        => rawurlencode( $plugin_name ),
+					'plugin_status' => 'all',
+					'paged'         => '1',
+					's'             => '',
+				),
+				admin_url( 'plugins.php' )
+			),
+			'deactivate-plugin_' . $plugin_name
+		);
+
+		echo '<div class="notice notice-error">';
+		echo '<p>';
+		/* translators: %s: wp docs */
+		echo wp_kses_post( sprintf( __( 'The "Brazilian Market on WooCommerce" plugin requires at least PHP 5.6 to work (<a href="%s">learn how to update</a>), update the PHP version of your server or disable the plugin to disable this notification.', 'woocommerce-extra-checkout-fields-for-brazil' ), 'https://wordpress.org/support/update-php/' ) );
+		echo '</p>';
+		echo '<p><a href="' . esc_url( $disable_url ) . '" class="button-primary">' . esc_html__( 'Disable "Brazilian Market on WooCommerce" plugin', 'woocommerce-extra-checkout-fields-for-brazil' ) . '</a></p>';
+		echo '</div>';
+	}
+
+	add_action( 'admin_notices', 'cs_brazilian_market_unsupported_php_version_notice' );
+	return;
 }
-
-if ( ! class_exists( 'Extra_Checkout_Fields_For_Brazil' ) ) :
-
-/**
- * Plugin main class.
- */
-class Extra_Checkout_Fields_For_Brazil {
-
-	/**
-	 * Plugin version.
-	 *
-	 * @var string
-	 */
-	const VERSION = '3.7.0';
-
-	/**
-	 * Instance of this class.
-	 *
-	 * @var object
-	 */
-	protected static $instance = null;
-
-	/**
-	 * Initialize the plugin.
-	 *
-	 */
-	private function __construct() {
-		// Load plugin text domain.
-		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
-
-		if ( class_exists( 'WooCommerce' ) ) {
-			if ( is_admin() ) {
-				$this->admin_includes();
-			}
-
-			$this->includes();
-			add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'plugin_action_links' ) );
-		} else {
-			add_action( 'admin_notices', array( $this, 'woocommerce_fallback_notice' ) );
-		}
-	}
-
-	/**
-	 * Return an instance of this class.
-	 *
-	 * @return object A single instance of this class.
-	 */
-	public static function get_instance() {
-		// If the single instance hasn't been set, set it now.
-		if ( null === self::$instance ) {
-			self::$instance = new self;
-		}
-
-		return self::$instance;
-	}
-
-	/**
-	 * Get assets url.
-	 *
-	 * @return string
-	 */
-	public static function get_assets_url() {
-		return plugins_url( 'assets/', __FILE__ );
-	}
-
-	/**
-	 * Load the plugin text domain for translation.
-	 */
-	public function load_plugin_textdomain() {
-		$locale = apply_filters( 'plugin_locale', get_locale(), 'woocommerce-extra-checkout-fields-for-brazil' );
-
-		load_textdomain( 'woocommerce-extra-checkout-fields-for-brazil', trailingslashit( WP_LANG_DIR ) . 'woocommerce-extra-checkout-fields-for-brazil/woocommerce-extra-checkout-fields-for-brazil-' . $locale . '.mo' );
-		load_plugin_textdomain( 'woocommerce-extra-checkout-fields-for-brazil', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-	}
-
-	/**
-	 * Includes.
-	 */
-	private function includes() {
-		include_once dirname( __FILE__ ) . '/includes/class-extra-checkout-fields-for-brazil-formatting.php';
-		include_once dirname( __FILE__ ) . '/includes/class-extra-checkout-fields-for-brazil-front-end.php';
-		include_once dirname( __FILE__ ) . '/includes/class-extra-checkout-fields-for-brazil-integrations.php';
-		include_once dirname( __FILE__ ) . '/includes/class-extra-checkout-fields-for-brazil-api.php';
-	}
-
-	/**
-	 * Admin includes.
-	 */
-	private function admin_includes() {
-		include_once dirname( __FILE__ ) . '/includes/admin/class-extra-checkout-fields-for-brazil-admin.php';
-		include_once dirname( __FILE__ ) . '/includes/admin/class-extra-checkout-fields-for-brazil-settings.php';
-		include_once dirname( __FILE__ ) . '/includes/admin/class-extra-checkout-fields-for-brazil-order.php';
-		include_once dirname( __FILE__ ) . '/includes/admin/class-extra-checkout-fields-for-brazil-customer.php';
-	}
-
-	/**
-	 * Action links.
-	 *
-	 * @param  array $links Default plugin links.
-	 *
-	 * @return array
-	 */
-	public function plugin_action_links( $links ) {
-		$plugin_links   = array();
-		$plugin_links[] = '<a href="' . esc_url( admin_url( 'admin.php?page=woocommerce-extra-checkout-fields-for-brazil' ) ) . '">' . __( 'Settings', 'woocommerce-extra-checkout-fields-for-brazil' ) . '</a>';
-
-		return array_merge( $plugin_links, $links );
-	}
-
-	/**
-	 * WooCommerce fallback notice.
-	 *
-	 * @return string Fallack notice.
-	 */
-	public function woocommerce_fallback_notice() {
-		echo '<div class="error"><p>' . sprintf( __( 'Brazilian Market on WooCommerce depends on %s to work!', 'woocommerce-extra-checkout-fields-for-brazil' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">' . __( 'WooCommerce', 'woocommerce-extra-checkout-fields-for-brazil' ) . '</a>' ) . '</p></div>';
-	}
-}
-
-/**
- * Initialize the plugin.
- */
-add_action( 'plugins_loaded', array( 'Extra_Checkout_Fields_For_Brazil', 'get_instance' ) );
-
-endif;
