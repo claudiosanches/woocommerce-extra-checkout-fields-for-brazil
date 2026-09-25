182b74677ffcf1e641614c8a96072e7d1e0a5612
Claudio Sanches <contato@claudiosanches.com>
2026-09-25 20:21:19 -0300

    Wrap the cart calculator entry to satisfy Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,10 @@ module.exports = {
 		frontend: path.resolve( __dirname, 'assets/js/frontend/frontend.js' ),
 		blocks: path.resolve( __dirname, 'assets/js/blocks/index.ts' ),
 		shipping: path.resolve( __dirname, 'assets/js/shipping/shipping.ts' ),
-		'shipping-cart': path.resolve( __dirname, 'assets/js/shipping/cart.ts' ),
+		'shipping-cart': path.resolve(
+			__dirname,
+			'assets/js/shipping/cart.ts'
+		),
 		'shipping-editor': path.resolve(
 			__dirname,
 			'assets/js/shipping/editor.ts'
